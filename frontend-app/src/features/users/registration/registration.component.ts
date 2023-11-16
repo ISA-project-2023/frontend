d31bf58679a044ee60f0914ec8bdfe66a80f5ac9
Lara <larapetkovic021@gmail.com>
2023-11-16 19:26:17 +0100

    feat: Employee registration implemented

--- frontend-app/src/features/users/registration/registration.component.ts
+++ frontend-app/src/features/users/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -6,6 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  username: string = '';
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
@@ -18,6 +21,8 @@ export class RegistrationComponent {
   companyInfo: string = '';
   passwordError: string = '';
 
+  constructor(private userService: UserService, private router: Router) {} 
+
   validateForm(): boolean {
     if (this.password !== this.confirmPassword) {
       this.passwordError = 'Passwords do not match';
@@ -25,6 +30,37 @@ export class RegistrationComponent {
     } else {
       this.passwordError = '';
       return true;
+    }
+  }
+
+  register(): void {
+    if (this.validateForm()) {
+      const employee = {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        city: this.city,
+        country: this.country,
+        phoneNumber: this.phoneNumber,
+        profession: this.profession,
+        companyInfo: this.companyInfo,
+        role: 'EMPLOYEE'
+      };
+
+      this.userService.saveUser(employee, this.password).subscribe(
+        response => {
+            console.log('Employee saved successfully', response);
+            this.router.navigate(['/home']);
+        },
+        error => {
+            console.error('Error saving employee', error);
+            alert('There was an error while saving the data! Please try again.');
+        }
+    );
+    
     }
   }
 
