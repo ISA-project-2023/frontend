26641926ad1827faaecaa055e49cf87786a9e35c
Lara <larapetkovic021@gmail.com>
2024-01-27 18:04:09 +0100

    feat: Map centered on Belgrade

--- frontend-app/src/features/map/map-simulator/map-simulator.component.ts
+++ frontend-app/src/features/map/map-simulator/map-simulator.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import * as olProj from 'ol/proj';
 
 @Component({
   selector: 'app-map-simulator',
   templateUrl: './map-simulator.component.html',
   styleUrls: ['./map-simulator.component.css']
 })
-export class MapSimulatorComponent {
+export class MapSimulatorComponent implements OnInit {
+  map!: Map;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+    this.initializeMap();
+  }
+
+  initializeMap() {
+    const initialCenter = olProj.fromLonLat([20.412597,44.807173]);
+    const initialZoom = 9;
+
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new Tile({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: initialCenter,
+        zoom: initialZoom
+      })
+    });
+  }
+  
+  
 }
