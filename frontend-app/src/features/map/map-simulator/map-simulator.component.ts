a90cb445fbc01629b84f2d4f789a30912e958522
Lara <larapetkovic021@gmail.com>
2024-01-27 18:24:37 +0100

    feat: Position of the delivery vehicle is implemented. Left to connect it with external applications

--- frontend-app/src/features/map/map-simulator/map-simulator.component.ts
+++ frontend-app/src/features/map/map-simulator/map-simulator.component.ts
@@ -4,6 +4,9 @@ import Tile from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import View from 'ol/View';
 import * as olProj from 'ol/proj';
+import Overlay from 'ol/Overlay';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-map-simulator',
@@ -12,17 +15,19 @@ import * as olProj from 'ol/proj';
 })
 export class MapSimulatorComponent implements OnInit {
   map!: Map;
+  longitude: number = 19.8335; // Pozicije markera (naseg dostavljaca)
+  latitude: number = 45.2671;
 
   constructor() {}
 
   ngOnInit(): void {
-    console.log('ngOnInit called');
     this.initializeMap();
+    this.addMarker([this.longitude, this.latitude], 'https://cdn-icons-png.flaticon.com/256/3855/3855480.png');
   }
 
   initializeMap() {
-    const initialCenter = olProj.fromLonLat([20.412597,44.807173]);
-    const initialZoom = 9;
+    const initialCenter = olProj.fromLonLat([20.412597, 44.807173]);
+    const initialZoom = 9.5;
 
     this.map = new Map({
       target: 'map',
@@ -37,6 +42,26 @@ export class MapSimulatorComponent implements OnInit {
       })
     });
   }
-  
-  
+
+  addMarker(coordinates: [number, number], imageUrl: string) {
+    const marker = new Overlay({
+      position: fromLonLat(coordinates),
+      positioning: 'center-center',
+      element: this.createMarkerElement(imageUrl),
+      stopEvent: false,
+    });
+
+    this.map.addOverlay(marker);
+  }
+
+  createMarkerElement(imageUrl: string): HTMLImageElement {
+    const element = new Image();
+    element.src = imageUrl;
+
+    element.style.width = '30px';
+    element.style.height = '30px';
+
+    element.className = 'map-marker';
+    return element;
+  }
 }
